238504a7d9d4ac77004685c3396b416dac2e6fa8
tnaldinho <naldinho@kth.se>
2025-09-23 10:32:49 +0200

    Add shift_register implementation, rename file

---
+++ q1/shift_register.sv
@@ -0,0 +1,25 @@
+module shift_register #(parameter N=4)
+                      (input logic clk,
+                       input logic rst_n,
+                       input logic serial_parallel,
+                       input logic load_enable,
+                       input logic serial_in,
+                       input logic [N-1:0] parallel_in,
+                       output logic [N-1:0] parallel_out,
+                       output logic serial_out);
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (load_enable) begin
+            if (serial_parallel) begin
+                // Parallel loading
+                parallel_out <= parallel_in;
+            end else begin
+                // Serial loading (shift left)
+                parallel_out <= {parallel_out[N-2:0], serial_in};
+            end
+        end
+    end
+    
+    assign serial_out = parallel_out[N-1];
+
+endmodule
